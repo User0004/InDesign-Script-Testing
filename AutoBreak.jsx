1d0413bbd97fd0b1ab2c3350414e89aa33aafa54
George User <george.hannaford.01@gmail.com>
2024-09-28 20:24:14 +0100

    Update AutoBreak.jsx

--- AutoBreak.jsx
+++ AutoBreak.jsx
@@ -2,7 +2,7 @@
 // STATUS: Ready 
 // FUNCTION: Splits a selected text frame with multiple columns into separate linked columns in Adobe InDesign, while preserving the text flow and layout. 
 // Note: The script will not work on sensitive first box database items for header, body, and captions.
-// Prefered keyboard shortcut is F5
+// Prefered keyboard shortcut is alt+F1
 // Version 1.0.0
 
 // AutoBreak
