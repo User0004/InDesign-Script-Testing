673c5ac83674d3ca8988863107e4ce2c0ef45fd3
George User <george.hannaford.01@gmail.com>
2024-09-28 20:19:35 +0100

    Update Bottom Shrink.jsx
    // Version 1.0.0

--- Bottom Shrink.jsx
+++ Bottom Shrink.jsx
@@ -3,6 +3,7 @@
 // FUNCTION: Decrease height of selected items from top one baseline for each time the script is run
 // Best to use on non group grouped items for Nav. If needed use white arrow to handle groups, but black arrow is find for text boxes 
 // Prefered keyboard shortcut is Bottom Shrink shift F10
+// Version 1.0.0
 
 // Bottom shrink
 app.doScript(function() {
