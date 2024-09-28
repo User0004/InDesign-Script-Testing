13c2accb392282788cbb06f298dd105b14f8c1f6
George User <george.hannaford.01@gmail.com>
2024-09-28 20:29:05 +0100

    Update Bottom Expand.jsx

--- Bottom Expand.jsx
+++ Bottom Expand.jsx
@@ -2,6 +2,7 @@
 // STATUS: Ready
 // FUNCTION: Increase height of selected items from bottom one baseline for each time the script is run
 // Best to use on non group grouped items for Nav. If needed use white arrow to handle groups, but black arrow is find for text boxes 
+// Author: George Hannaford george.hannaford@telegraph.co.uk
 // Prefered keyboard shortcut is alt+F11
 // Version 1.0.0
 
